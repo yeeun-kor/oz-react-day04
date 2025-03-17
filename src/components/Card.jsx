b32175f40b4327bedc5dc15df8d9951e5ee7942a
Kim Minsu <118744078+yoyobar@users.noreply.github.com>
2025-03-17 21:36:55 +0900

    fix: 주석 수정

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,6 +2,7 @@ export default function Card({ item }) {
     //1. item을 받아와서 카드를 렌더링 해야 합니다.
     //2. item의 title, description, category를 각각 h2, p, span태그에 알맞게 넣으세요
     //3. item은 CardList에서 전달됩니다.
+    //* 먼저 CardList.jsx를 작업해야 합니다!
 
     return (
         <div className="card">
--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -5,6 +5,7 @@ export default function CardList({ data: listData }) {
     //1. listData를 받아와서 카드 리스트를 렌더링 해야 합니다.
     //2. listData의 각 요소를 Card 컴포넌트에 props로 넘겨주어야 합니다.
     //3. Card 컴포넌트에는 item을 넘겨주어야 합니다.
+    //4. item은 listData 요소입니다 console.log로 찍히는 listData의 정보를 확인해보세요.
 
     return (
         <div className="card-grid">
