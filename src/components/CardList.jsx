fce307cbcbc405535e6fd6541db2fa0d9a94e858
Kim Minsu <118744078+yoyobar@users.noreply.github.com>
2025-03-27 22:16:28 +0900

    fix: Update CardList.jsx

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -6,7 +6,7 @@ export default function CardList({ data: listData }) {
     //2. listData의 각 요소를 Card 컴포넌트에 props로 넘겨주어야 합니다.
     //3. Card 컴포넌트에는 item을 넘겨주어야 합니다.
     //4. item은 listData 요소입니다 console.log로 찍히는 listData의 정보를 확인해보세요.
-
+    //5. Card 컴포넌트를 map을 이용해 여러번 렌더링할 때, Card에 key를 설정해야합니다.
     return (
         <div className="card-grid">
             <Card />
