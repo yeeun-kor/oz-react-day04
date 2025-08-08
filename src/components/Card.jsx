457472be6792f2463757ef336d4d19d8d21ec88b
yeeun <jovm268@naver.com>
2025-08-08 15:21:05 +0900

    과제 제출

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,15 @@
-export default function Card({ item }) {
-    //1. item을 받아와서 카드를 렌더링 해야 합니다.
-    //2. item의 title, description, category를 각각 h2, p, span태그에 알맞게 넣으세요
-    //3. item은 CardList에서 전달됩니다.
-    //* 먼저 CardList.jsx를 작업해야 합니다!
-
+export default function Card({ item: { category, title, description } }) {
     return (
+        //구조분해 할당 사용
+        //item :{title:, category:,description:}으로 꺼내왔다.
         <div className="card">
             <div className="card-image">
                 <img src={`https://placehold.co/600x400`} />
             </div>
             <div className="card-content">
-                <span className="card-category">???</span>
-                <h2 className="card-title">???</h2>
-                <p className="card-description">???</p>
+                <span className="card-category">{category}</span>
+                <h2 className="card-title">{title}</h2>
+                <p className="card-description">{description}</p>
             </div>
         </div>
     );
